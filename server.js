173660c05bb8ee51d2f78548dd285be68fc36138
alexanderscher <alexcscher@gmail.com>
2023-04-12 16:37:28 -0700

    add employee function

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3001;
 const db = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: process.env.DB_PASSWORD,
-  password: process.env.DB_USER,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
@@ -60,8 +60,8 @@ function addDepartment() {
 }
 
 function addRole() {
-  const query = "SELECT * FROM departments";
-  db.query(query, (err, res) => {
+  const query1 = "SELECT * FROM departments";
+  db.query(query1, (err, res) => {
     if (err) throw err;
     inquirer
       .prompt([
@@ -86,9 +86,9 @@ function addRole() {
         const department = res.find(
           (department) => department.name === answers.department
         );
-        const query = "INSERT INTO roles SET ?";
+        const query2 = "INSERT INTO roles SET ?";
         db.query(
-          query,
+          query2,
           {
             title: answers.title,
             salary: answers.salary,
@@ -108,33 +108,71 @@ function addRole() {
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
 function addEmployee() {
-  // Retrieve list of roles from the database]
   db.query("SELECT id, title FROM roles", (error, results) => {
     if (error) {
       console.error(error);
       return;
     }
+
     const roles = results.map(({ id, title }) => ({
       name: title,
       value: id,
     }));
-    console.log(roles);
-  });
+    db.query(
+      'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employees',
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
 
-  db.query(
-    'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employees',
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        return;
+        const managers = results.map(({ id, name }) => ({
+          name,
+          value: id,
+        }));
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "firstName",
+              message: "Enter employees first name:",
+            },
+            {
+              type: "input",
+              name: "lastName",
+              message: "Enter employees last name:",
+            },
+            {
+              type: "list",
+              name: "roleID",
+              message: "Select the department for the new role:",
+              choices: roles,
+            },
+            {
+              type: "list",
+              name: "managerId",
+              message: "Select the employee manager:",
+              choices: managers,
+            },
+          ])
+          .then((answers) => {
+            query =
+              "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+            const values = [
+              (first_name = answers.firstName),
+              (last_name = answers.lastName),
+              (role_id = answers.roleID),
+              (manager_id = answers.managerId),
+            ];
+            db.query(query, values, (err, res) => {
+              if (err) throw err;
+              console.log("Employee added successfully");
+            });
+          });
       }
-      const managers = results.map(({ name, id }) => ({
-        name,
-        value: id,
-      }));
-      console.log(managers);
-    }
-  );
+    );
+  });
 }
 
 addEmployee();
+viewEmployees();
